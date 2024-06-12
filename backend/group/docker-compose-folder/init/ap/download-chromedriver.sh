88f0002f28ca7bd7c715fd9965ef2fd73be605e5
ISAAC\a2640 <issac.hsu@infotech.com.tw>
2024-06-12 11:03:27 +0800

    調整路徑

--- backend/group/docker-compose-folder/init/ap/download-chromedriver.sh
+++ backend/group/docker-compose-folder/init/ap/download-chromedriver.sh
@@ -1,5 +1,6 @@
 !bin#bash
-cd ../../../schedule/chrome-driver
+mkdir -p ../../../schedule/chrome-driver/chromedriver-linux64
+cd ../../../schedule/chrome-driver/chromedriver-linux64
 curl -o chromedriver-linux64.zip https://storage.googleapis.com/chrome-for-testing-public/125.0.6422.141/linux64/chromedriver-linux64.zip
 unzip chromedriver-linux64.zip
 
