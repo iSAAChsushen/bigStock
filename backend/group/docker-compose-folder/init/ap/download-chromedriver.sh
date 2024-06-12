2cbc5cc45eae9521888614de849c7aee8fb3fc62
ISAAC\a2640 <issac.hsu@infotech.com.tw>
2024-06-12 09:38:37 +0800

    add use docker-compose way to setup

---
+++ backend/group/docker-compose-folder/init/ap/download-chromedriver.sh
@@ -0,0 +1,4 @@
+!bin#bash
+cd ../../../schedule/chrome-driver
+curl -o chromedriver-linux64.zip https://storage.googleapis.com/chrome-for-testing-public/125.0.6422.141/linux64/chromedriver-linux64.zip
+unzip chromedriver-linux64.zip
