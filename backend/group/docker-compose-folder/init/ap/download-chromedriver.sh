cdc23a19654e484708608a93669fa5c5483725ad
ISAAC\a2640 <issac.hsu@infotech.com.tw>
2024-06-12 10:30:03 +0800

    add maven build process

--- backend/group/docker-compose-folder/init/ap/download-chromedriver.sh
+++ backend/group/docker-compose-folder/init/ap/download-chromedriver.sh
@@ -2,3 +2,4 @@
 cd ../../../schedule/chrome-driver
 curl -o chromedriver-linux64.zip https://storage.googleapis.com/chrome-for-testing-public/125.0.6422.141/linux64/chromedriver-linux64.zip
 unzip chromedriver-linux64.zip
+
---
+++ backend/group/docker-compose-folder/init/ap/maven-build.sh
@@ -0,0 +1,4 @@
+!bin#bash
+cd ../../../
+
+maven clean install -DskipTests=true
